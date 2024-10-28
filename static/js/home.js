2d0e730e692b4099e555ce66fdc2df744e083f21
MonkieeBoi <danny.pham26425@gmail.com>
2024-10-28 15:55:41 +1100

    Perf: avoid unnecessary date parsing

--- static/js/home.js
+++ static/js/home.js
@@ -56,23 +56,25 @@ intervals = new Set();
 window.onload = () => {
     document.querySelectorAll(".item-due").forEach(element => {
         interval = setInterval(() => {
-            let dueDate = new Date(element.dataset.date);
+            let dueDate = element.dataset.date;
             if (!("date" in element.dataset)) {
                 let date, time;
                 [date, time] = element.parentElement.querySelector(".item-date").textContent.split(" ");
                 date = date.split("/");
                 time = time.split(":");
-                dueDate = new Date(date[2], date[1] - 1, date[0], time[0], time[1]);
+                dueDate = new Date(date[2], date[1] - 1, date[0], time[0], time[1]).getTime();
                 element.dataset.date = dueDate;
             }
             let now = new Date().getTime();
 
             let distance = dueDate - now;
+            distance = Math.floor(distance / 1000);
+            console.log(distance);
 
-            let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            let days = Math.floor(distance / (60 * 60 * 24));
+            let hours = Math.floor((distance % (60 * 60 * 24)) / (60 * 60));
+            let minutes = Math.floor((distance % (60 * 60)) / (60));
+            let seconds = Math.floor((distance % (60)));
 
             element.innerHTML = "Due: "
                 + (days > 0 ? days + "d " : "")
